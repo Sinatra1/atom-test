3e152b2411ba23798955f3514bc668be2dad2c5f
shumilov <shumilov@simpls.ru>
2019-02-10 11:52:48 +0500

    added created_user_id to book

--- migrations/m190204_173236_create_table_book.php
+++ migrations/m190204_173236_create_table_book.php
@@ -2,6 +2,7 @@
 
 use yii\db\Migration;
 use app\models\Book;
+use app\models\User;
 
 /**
  * Class m190204_173236_create_table_book
@@ -13,7 +14,8 @@ class m190204_173236_create_table_book extends Migration
     public static $name = 'name';
     public static $year = 'year';
     public static $isbn = 'isbn';
-
+    public static $createdUserId = 'created_user_id';
+    
     /**
      * create table book
      */
@@ -25,6 +27,7 @@ class m190204_173236_create_table_book extends Migration
             'updated' => $this->dateTime()->notNull()->defaultExpression('NOW()'),
             'deleted' => $this->dateTime()->null(),
             'is_deleted' => $this->boolean()->notNull()->defaultValue('0'),
+            self::$createdUserId => $this->integer(11)->notNull(),
             self::$isbn => $this->string(13)->unique()->null(),
             self::$year => $this->integer(4)->notNull(),
             self::$name => $this->string()->notNull(),
@@ -32,9 +35,13 @@ class m190204_173236_create_table_book extends Migration
             'description' => $this->text()->null()
                 ], 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB');
 
+        $this->createIndex('idx_' . Book::tableName() . '_' . self::$createdUserId, Book::tableName(), self::$createdUserId);
+        
         $this->createIndex('idx_' . self::$tableName . '_name', Book::tableName(), 'name');
 
         $this->createIndex('idx_' . self::$tableName . '_' . self::$name. '_' . self::$year, Book::tableName(), [self::$name, self::$year], true);
+        
+        $this->addForeignKey('fk_' . User::tableName() . '_' . self::$createdUserId, Book::tableName(), self::$createdUserId, User::tableName(), 'id');
     }
 
     /**
--- models/Book.php
+++ models/Book.php
@@ -3,12 +3,14 @@
 namespace app\models;
 
 use yii\web\ServerErrorHttpException;
+use Yii;
 
 /**
  * This is the model class for table "book".
  *
  * @property integer $id
  * @property string $name
+ * @property integer $created_user_id 
  * @property string $isbn
  * @property integer $year
  * @property string $cover_image
@@ -45,12 +47,12 @@ class Book extends Base
 
         return [
             [['name', 'year'], 'required'],
+            [['name', 'year', 'isbn', 'description'], 'trim'],
             [['name', 'cover_image'], 'string', 'max' => 255],
             ['isbn', 'string', 'length' => 13],
             [['year', 'isbn'], 'integer'],
             ['year', 'compare', 'compareValue' => $currentYear, 'operator' => '<=', 'type' => 'number'],
             ['year', 'compare', 'compareValue' => $oldestYear, 'operator' => '>=', 'type' => 'number'],
-            [['name', 'year', 'isbn', 'description'], 'trim'],
             ['description', 'string'],
             ['isbn', 'unique'],
             [['name', 'year'], 'unique', 'targetAttribute' => ['name', 'year']],
@@ -72,6 +74,10 @@ class Book extends Base
         if (empty($this->cover_image)) {
             $this->cover_image = null;
         }
+        
+        if ($this->isNewRecord) {
+            $this->created_user_id = Yii::$app->user->id;
+        }
 
         return $result;
     }
@@ -87,8 +93,8 @@ class Book extends Base
             //throw new ServerErrorHttpException('Uploaded file is too big');
             return false;
         }
-        $parentDir = \Yii::$app->basePath . '/web';
-        $uploadDirPath = $parentDir . '/' . \Yii::$app->params['uploadDirName'];
+        $parentDir = Yii::$app->basePath . '/web';
+        $uploadDirPath = $parentDir . '/' . Yii::$app->params['uploadDirName'];
 
         if (!file_exists($uploadDirPath)) {
             throw new ServerErrorHttpException('Upload dir ' . $uploadDirPath . ' not exists');
@@ -135,7 +141,7 @@ class Book extends Base
             $userToBook = new UserToBook();
             $userToBookQuery = $userToBook->getListQuery()->
                     select('book_id')->
-                    andWhere(['user_id' => \Yii::$app->user->id])->
+                    andWhere(['user_id' => Yii::$app->user->id])->
                     asArray();
 
             $query->andWhere(['in', 'id', $userToBookQuery]);
